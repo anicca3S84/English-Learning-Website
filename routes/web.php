11e7437a67ae79d9c671499045648e1978b603a2
Cuongcuch <nmanhcuong.cpf@gmail.com>
2025-04-21 10:27:36 +0700

    add sign in page (make interface)

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('layout.index');
 });
+
+Route::get('/login', function () {
+    return view('layout.login');
+})->name('login'); 
