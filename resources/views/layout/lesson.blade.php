4716d35c8e1569f068dc6cc4877f9e5689ce2551
anicca <anicca3584@gmail.com>
2025-04-22 21:46:10 +0700

    task 1 complete

--- resources/views/layout/lesson.blade.php
+++ resources/views/layout/lesson.blade.php
@@ -23,7 +23,6 @@
 
 <body>
     <div class="page-wrapper">
-        <!-- Include header -->
         @include('partial.header')
 
         <div class="container mt-5 mb-5">
@@ -255,7 +254,6 @@
 
 
 
-    <!-- Optional JS: jQuery, Popper.js, and Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
